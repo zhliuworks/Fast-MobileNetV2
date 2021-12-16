f101f5236bea08082bbea69b0abe20dd92463b7b
zhliuworks <zihanliu2000@gmail.com>
2021-12-16 20:14:42 +0800

    remove main.cu

--- mobilenet_v2/main.cu
+++
@@ -1,8 +0,0 @@
-#include "fast_mobilenet/fastMobileNetV2.cuh"
-#include "cudnn/cudnnMobileNetV2.cuh"
-
-__host__ int main() {
-    // compare our cuda kernels and cudnn in the inference of MobileNetV2
-    // accuracy and performance count
-    return 0;
-}
